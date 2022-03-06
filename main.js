500f785ec2a8c93804f06eeb75800e82b5f7fc8f
FireRubies <40702001+FireRubies@users.noreply.github.com>
2022-03-06 09:17:29 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+//4 Strings
+var name = "Kyler"
+var world = "Zyria"
+var location = "Robia Village"
+var currency = "gold"
+
+//3 Integers
+var age = 17
+var money = 100
+var year = 1564
+
+//1 Array
+var foods = ["Pie", "Pizza", "Cake", "Muffins"]
+
+//1 Boolean
+var hasAwoken = false
+
+console.log(`Welcome to the world of ${world}, ${name}!`);
+console.log(`You're currently in ${location} in the year ${year}.`);
+console.log(`Our spell formation tells us some data about you; Age: ${age}, Awoken: ${hasAwoken}, Favorite Food: ${foods[Math.floor(Math.random() * foods.length)]}`);
+console.log("Enjoy your time in our world Hero! Don't die!");
